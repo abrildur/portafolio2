8517478e6574a88848fb0a479dc6c181cd9b79c8
Abril <71578178+abrildur@users.noreply.github.com>
2023-04-21 02:14:33 +0000

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Abril",
+  title: "Cloud Advocate en Microsoft",
+  email: "abrilu@example.com",
+  gitHub: "abrilurena",
+  linkedIn: "abrilurena",
+  twitter: "abrildaur",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,9 +20,9 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/server-wall.jpg";
+import image from "../images/foto1.jpg";
 
-const imageAltText = "woman holding laptop standing by server room with glass wall";
+const imageAltText = "a woman writing in her laptop";
 
 const Home = ({ name, title }) => {
   return (
